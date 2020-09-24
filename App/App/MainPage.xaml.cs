7296b84d149e623de41593e01687abb0237e3e24
ketchiboy <simonkemeter@yahoo.de>
2020-09-24 18:27:42 +0200

    Projektdateien hinzufügen.

---
+++ App/App/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+namespace PageNavigation
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void NavigateButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SecondPage());
+        }
+    }
+}
---
+++ App/App/SecondPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Xamarin.Forms;
+namespace PageNavigation
+{
+    public partial class SecondPage : ContentPage
+    {
+        public SecondPage()
+        {
+            InitializeComponent();
+            MySlider.ValueChanged += MySlider_ValueChanged;
+        }
+
+     
+
+        private void MySlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double value1 = MySlider.Value;
+            int value2 = (int)value1;
+            MyLabel.Text = "N= [" + value2.ToString() + "]";
+
+            int fib = Fibonacci(value2);
+
+            MyLabel2.Text = "fib= [" + fib.ToString() + "]";
+        }
+
+        public static int Fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+
+
+    }
+}
